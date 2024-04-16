4d5371d2be23e1f9b31e69a52015b153f9a19ba0
oddy-bassey <oddybassey@gmail.com>
2024-04-16 13:13:13 +0100

    initial project setup

---
+++ src/test/java/com/example/farmcollector/FarmCollectorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.farmcollector;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FarmCollectorApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
