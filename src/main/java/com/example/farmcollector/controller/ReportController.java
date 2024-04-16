b819c6a6446fb866d8812fe96e185e3478404a60
oddy-bassey <oddybassey@gmail.com>
2024-04-16 16:18:55 +0100

    implemented the farm and report controller

---
+++ src/main/java/com/example/farmcollector/controller/ReportController.java
@@ -0,0 +1,2 @@
+package com.example.farmcollector.controller;public class ReportController {
+}
---
+++ src/main/java/com/example/farmcollector/service/ReportServiceImpl.java
@@ -0,0 +1,15 @@
+package com.example.farmcollector.service;
+
+import java.util.Map;
+
+public class ReportServiceImpl implements ReportService {
+    @Override
+    public Map<String, Double> getExpectedVsActualProductForFarm(Long farmId) {
+        return null;
+    }
+
+    @Override
+    public Map<String, Double> getExpectedVsActualProductForCropType(String cropType) {
+        return null;
+    }
+}
